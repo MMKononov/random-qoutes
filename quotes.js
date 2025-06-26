29899936f800b9ccd01d113801501cd02761fac6
Mikhail Kononov <mmkononov@yandex.ru>
2025-06-26 14:16:51 +0300

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,25 @@
+const quotes = [
+  {
+    qoute: "Be yourself; everyone else is already taken",
+    author: "Oscar Wilde",
+  },
+  {
+    qoute:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts",
+    author: "Winston Churchill",
+  },
+  {
+    qoute: "Code is like humor. When you have to explain it, it’s bad",
+    author: "Cory House",
+  },
+  {
+    qoute: "In the middle of difficulty lies opportunity",
+    author: "Albert Einstein",
+  },
+  {
+    qoute: "First, solve the problem. Then, write the code",
+    author: "John Johnson",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-const quotes = [
-  {
-    qoute: "Be yourself; everyone else is already taken",
-    author: "Oscar Wilde",
-  },
-  {
-    qoute:
-      "Success is not final, failure is not fatal: it is the courage to continue that counts",
-    author: "Winston Churchill",
-  },
-  {
-    qoute: "Code is like humor. When you have to explain it, it’s bad",
-    author: "Cory House",
-  },
-  {
-    qoute: "In the middle of difficulty lies opportunity",
-    author: "Albert Einstein",
-  },
-  {
-    qoute: "First, solve the problem. Then, write the code",
-    author: "John Johnson",
-  },
-];
-
+import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
@@ -29,13 +6,14 @@ const generateBtn = document.getElementById("generate-btn");
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  // OPTION 1: Destructuring
-  // const { qoute, author: quoteAuthor } = randomQuote;
-  // OPTION 2: Direct assignment
-  // const qoute = randomQuote.qoute;
-  // const quoteAuthor = randomQuote.author;
   // OPTION 3: Using dot notation
   quoteElement.textContent = randomQuote.qoute;
   quoteAuthorElement.textContent = randomQuote.author;
 }
 generateBtn.addEventListener("click", generateRandomQuote);
+
+// OPTION 1: Destructuring
+// const { qoute, author: quoteAuthor } = randomQuote;
+// OPTION 2: Direct assignment
+// const qoute = randomQuote.qoute;
+// const quoteAuthor = randomQuote.author;
