fa1f9d8601b2c4de34b6f0b020e90b3b9959f627
Mikhail Kononov <mmkononov@yandex.ru>
2025-06-27 15:03:17 +0300

    generateRandomInt util function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import quotes from "./src/quotes.js";
+import { generateRandomInt } from "./src/utils/utils.js";
 import {
   toggleFavoriteIcon,
   showFavoriteCard,
@@ -14,8 +15,10 @@ const favoritesContainer = document.getElementById("favorites-container");
 let currentQuoteIndex;
 
 function generateRandomQuote() {
-  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[currentQuoteIndex];
+  const randomIndex = generateRandomInt(quotes.length);
+  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = randomIndex;
+
   const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
