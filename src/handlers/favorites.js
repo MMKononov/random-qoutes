953dca22c8be2f67124773983ffedf849d1ae86e
Mikhail Kononov <mmkononov@yandex.ru>
2025-07-06 19:55:27 +0300

    Remove specific favorite quote

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,4 +1,4 @@
-import { favoriteBtn } from '../../index.js';
+import { quoteFavoriteBtn } from '../../index.js';
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
@@ -7,41 +7,55 @@ function toggleFavorite(quote, btn, container) {
   if (quote.isFavorite) {
     showFavoriteCard(quote, container);
   } else {
-    hideFavoriteCard(quote.id);
+    removeFavoriteCard(quote.id);
   }
 }
 
 function handleFavorite(isFavorite) {
-  showFavoriteBtn(favoriteBtn);
-  toggleFavoriteBtnIcon(isFavorite, favoriteBtn);
+  showFavoriteBtn();
+  toggleFavoriteBtnIcon(isFavorite);
 }
 
-function toggleFavoriteBtnIcon(isFavorite, el) {
-  el.classList.toggle('fa-solid', isFavorite);
-  el.classList.toggle('fa-regular', !isFavorite);
+function toggleFavoriteBtnIcon(isFavorite) {
+  quoteFavoriteBtn.classList.toggle('fa-solid', isFavorite);
+  quoteFavoriteBtn.classList.toggle('fa-regular', !isFavorite);
 }
-function showFavoriteBtn(btn) {
-  btn.style.display = 'inline-block';
+function showFavoriteBtn() {
+  quoteFavoriteBtn.style.display = 'inline-block';
 }
 
-function hideFavoriteBtn(btn) {
-  btn.style.display = 'none';
+function hideFavoriteBtn() {
+  quoteFavoriteBtn.style.display = 'none';
+}
+
+function removeFavoriteQuote(quote) {
+  quote.isFavorite = false;
+  removeFavoriteCard(quote.id);
+  const currentQuote = document.querySelector('[data-current-quote-id]');
+  const currentQuoteId = currentQuote.dataset.currentQuoteId;
+  if (currentQuoteId === quote.id) {
+    toggleFavoriteBtnIcon(quote.isFavorite);
+  }
 }
 
 function showFavoriteCard(quote, container) {
   const { id, text, author } = quote;
   const favoriteCard = document.createElement('div');
   favoriteCard.classList.add('favorite-card');
-  favoriteCard.dataset.quoteId = id;
+  favoriteCard.dataset.favoriteQuoteId = id;
   favoriteCard.innerHTML = `
   <p>${text}</p>
-  <p class="author">${author}</p>
+  <p class="favorite-card-author">${author}</p>
+  <button class="btn btn-danger"><i class="far fa-trash-alt"></i> Remove from favorites</button>
   `;
   container.appendChild(favoriteCard);
+
+  const removeButton = favoriteCard.querySelector('.btn-danger');
+  removeButton.addEventListener('click', () => removeFavoriteQuote(quote));
 }
 
-function hideFavoriteCard(id) {
-  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`);
+function removeFavoriteCard(id) {
+  const card = document.querySelector(`[data-favorite-quote-id="${id}"]`);
 
   if (card) {
     card.remove();
--- src/handlers/quote.js
+++ src/handlers/quote.js
@@ -1,5 +1,5 @@
-import { generateRandomInt } from "../utils/utils.js";
-import { handleFavorite } from "./favorites.js";
+import { generateRandomInt } from '../utils/utils.js';
+import { handleFavorite } from './favorites.js';
 
 function handleQuote(quotes, setCurrentQuote) {
   const randomQuote = choseRandomQuote(quotes);
@@ -8,10 +8,12 @@ function handleQuote(quotes, setCurrentQuote) {
 }
 
 function displayQuote(quote) {
-  const { text, author, isFavorite } = quote;
-  const quoteElement = document.getElementById("quote");
-  const quoteAuthorElement = document.getElementById("quote-author");
-  quoteElement.textContent = text;
+  const { id, text, author, isFavorite } = quote;
+  const quoteElement = document.getElementById('quote');
+  const quoteTextElement = document.getElementById('quote-text');
+  const quoteAuthorElement = document.getElementById('quote-author');
+  quoteElement.dataset.currentQuoteId = id;
+  quoteTextElement.textContent = text;
   quoteAuthorElement.textContent = author;
   handleFavorite(isFavorite);
 }
