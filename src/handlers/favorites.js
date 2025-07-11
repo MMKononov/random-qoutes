0357a49ffe4d52eef1607f282cf71a03b8a4fed8
Mikhail Kononov <mmkononov@yandex.ru>
2025-07-11 16:17:51 +0300

    firsts steps in synchronizing app state and localStorage (currentQuote and favoriteQuotes)

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,11 +1,12 @@
-import { quoteFavoriteBtn } from '../../index.js';
+import { quoteFavoriteBtn } from "../../index.js";
 
-function toggleFavorite(quote, btn, container) {
-  quote.isFavorite = !quote.isFavorite;
+function toggleFavorite(quote, setCurrentQuote, btn, container) {
+  const shouldToggleIsFavorite = true;
+  setCurrentQuote(quote, shouldToggleIsFavorite);
   toggleFavoriteBtnIcon(quote.isFavorite, btn);
 
   if (quote.isFavorite) {
-    showFavoriteCard(quote, container);
+    showFavoriteCard(quote, setCurrentQuote, container);
   } else {
     removeFavoriteCard(quote.id);
   }
@@ -17,31 +18,32 @@ function handleFavorite(isFavorite) {
 }
 
 function toggleFavoriteBtnIcon(isFavorite) {
-  quoteFavoriteBtn.classList.toggle('fa-solid', isFavorite);
-  quoteFavoriteBtn.classList.toggle('fa-regular', !isFavorite);
+  quoteFavoriteBtn.classList.toggle("fa-solid", isFavorite);
+  quoteFavoriteBtn.classList.toggle("fa-regular", !isFavorite);
 }
 function showFavoriteBtn() {
-  quoteFavoriteBtn.style.display = 'inline-block';
+  quoteFavoriteBtn.style.display = "inline-block";
 }
 
 function hideFavoriteBtn() {
-  quoteFavoriteBtn.style.display = 'none';
+  quoteFavoriteBtn.style.display = "none";
 }
 
-function removeFavoriteQuote(quote) {
-  quote.isFavorite = false;
+function removeFavoriteQuote(quote, setCurrentQuote) {
+  const shouldToggleIsFavorite = true;
+  setCurrentQuote(quote, shouldToggleIsFavorite);
   removeFavoriteCard(quote.id);
-  const currentQuote = document.querySelector('[data-current-quote-id]');
+  const currentQuote = document.querySelector("[data-current-quote-id]");
   const currentQuoteId = currentQuote.dataset.currentQuoteId;
   if (currentQuoteId === quote.id) {
     toggleFavoriteBtnIcon(quote.isFavorite);
   }
 }
 
-function showFavoriteCard(quote, container) {
+function showFavoriteCard(quote, setCurrentQuote, container) {
   const { id, text, author } = quote;
-  const favoriteCard = document.createElement('div');
-  favoriteCard.classList.add('favorite-card');
+  const favoriteCard = document.createElement("div");
+  favoriteCard.classList.add("favorite-card");
   favoriteCard.dataset.favoriteQuoteId = id;
   favoriteCard.innerHTML = `
   <p>${text}</p>
@@ -50,8 +52,10 @@ function showFavoriteCard(quote, container) {
   `;
   container.appendChild(favoriteCard);
 
-  const removeButton = favoriteCard.querySelector('.btn-danger');
-  removeButton.addEventListener('click', () => removeFavoriteQuote(quote));
+  const removeButton = favoriteCard.querySelector(".btn-danger");
+  removeButton.addEventListener("click", () =>
+    removeFavoriteQuote(quote, setCurrentQuote)
+  );
 }
 
 function removeFavoriteCard(id) {
@@ -62,4 +66,4 @@ function removeFavoriteCard(id) {
   }
 }
 
-export { handleFavorite, toggleFavorite, hideFavoriteBtn };
+export { handleFavorite, toggleFavorite, hideFavoriteBtn, showFavoriteCard };
